06aedf624e57fc735281a53074e3734fceb9f773
Muhammad Muneeb ur Rehman <m.muneeb.ur.rehman.2000@gmail.com>
2024-07-31 03:18:39 +0500

    Project template Structure Done

--- mini_templates/views.py
+++ mini_templates/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("it works")
+    return render(request=request, template_name='mini_templates/home.html')
