fa6534830302910c788333cc2fc1f848803acd19
Muhammad Muneeb ur Rehman <m.muneeb.ur.rehman.2000@gmail.com>
2024-08-02 02:15:54 +0500

    Mini Project 1 Done

--- mini_templates/urls.py
+++ mini_templates/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 import mini_templates.views as mini_view
 urlpatterns = [
-    path('', mini_view.index)
+    path('', mini_view.home, name="home"),
+    path('mini_1', mini_view.mini_1, name="mini_1")
 ]
--- mini_templates/views.py
+++ mini_templates/views.py
@@ -2,5 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def index(request):
+def home(request):
     return render(request=request, template_name='mini_templates/home.html')
+
+def mini_1(request):
+    return render(request=request, template_name='mini_templates/mini_1.html')
