af362f40d55821fe8d7a77a98c82885245d33b7a
Muneeb <m.muneeb.ur.rehman.2000@gmail.com>
2024-09-08 20:45:01 +0500

    update - project 5: static file and tailwind config done

--- theme/static_src/tailwind.config.js
+++ theme/static_src/tailwind.config.js
@@ -4,6 +4,7 @@ module.exports = {
     "../../templates/**/*.html",
     "../../**/templates/**/*.html",
   ],
+  darkMode: 'class',
   theme: {
     screens: {
       sm: "480px",
@@ -17,13 +18,33 @@ module.exports = {
         lightBlue: "hsl(233, 100%, 69%)",
         darkGrayishBlue: "hsl(210, 10%, 33%)",
         grayishBlue: "hsl(201, 11%, 66%)",
+        darkBlue: 'hsl(217, 28%, 15%)',
+        darkBlue1: 'hsl(218, 28%, 13%)',
+        darkBlue2: 'hsl(216, 53%, 9%)',
+        darkBlue3: 'hsl(219, 30%, 18%)',
+        accentCyan: 'hsl(176, 68%, 64%)',
+        accentBlue: 'hsl(198, 60%, 50%)',
+        lightRed: 'hsl(0, 100%, 63%)',
       },
       fontFamily: {
         montserrat: ["Montserrat", "sans-serif"],
         baijamjuree: ["Bai Jamjuree", "sans-serif"],
         josefin: ["Josefin Sans", "sans-serif"],
         alata: ["Alata"],
+        raleway: ['Raleway', 'sans-serif'],
+        opensans: ['Open Sans', 'sans-serif'],
       },
+      backgroundImage: (theme) => ({
+        'logo-dark-mode': "url('/static/projects/project_5/images/logo-dark-mode.svg')",
+        'logo-light-mode': "url('/static/projects/project_5/images/logo-light-mode.svg')",
+        'curvy-dark-mode': "url('/static/projects/project_5/images/bg-curvy-dark-mode.svg')",
+        'curvy-light-mode': "url('/static/projects/project_5/images/bg-curvy-light-mode.svg')",
+      }),
+    },
+  },
+  variants: {
+    extend: {
+      backgroundImage: ['dark'],
     },
   },
   plugins: [
